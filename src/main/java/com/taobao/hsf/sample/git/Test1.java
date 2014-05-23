17183377d9491452f13484f5120fc96c410154b4
xinmofei <xinmofei@163.com>
2014-05-23 17:38:27 +0800

    commit

--- src/main/java/com/taobao/hsf/sample/git/Test1.java
+++ src/main/java/com/taobao/hsf/sample/git/Test1.java
@@ -15,6 +15,6 @@ package com.taobao.hsf.sample.git;
 public class Test1 {
 
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("11");
     }
 }
